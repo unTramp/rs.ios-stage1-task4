3111e5fa4cc00f00febce926705b364660850615
unTramp <57003672+unTramp@users.noreply.github.com>
2020-04-12 22:43:14 +0300

    feat: add task4.2 answer

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,29 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    NSMutableArray* result = [self decomposer:[number integerValue] remain:[number integerValue] * [number integerValue]];
+    if (result == nil) {
+        return nil;
+    } else {
+        [result removeLastObject];
+        return result;
+    }
+}
+
+
+- (NSMutableArray*)decomposer:(NSInteger)n remain:(NSInteger)input {
+    if (input == 0) {
+        return [NSMutableArray arrayWithObject:[NSNumber numberWithInteger:n]];
+    }
+    for(NSInteger index = n - 1; index > 0; index--) {
+        if ((input - index * index) >= 0) {
+            NSMutableArray* r = [self decomposer:index remain:(input - (index * index))];
+            if (r != nil) {
+                [r addObject:[NSNumber numberWithInteger:n]];
+                return r;
+            }
+        }
+    }
+    return nil;
 }
 @end
