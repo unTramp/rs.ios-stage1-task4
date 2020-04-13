974abcb5b755dae032ebfd8e95ae0edb3f30c7a3
unTramp <57003672+unTramp@users.noreply.github.com>
2020-04-13 23:56:10 +0300

    feat: task4.2 WIP
    Keep working on sorting method. Added fool exeptions for now sorry

--- RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.h
+++ RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.h
@@ -2,4 +2,8 @@
 
 @interface FullBinaryTrees : NSObject
 - (NSString *)stringForNodeCount:(NSInteger)count;
+
+@property (nonatomic, strong) NSMutableArray* resultNodeValuesArray;
+@property (nonatomic, assign) NSInteger counter;
+
 @end
--- RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
+++ RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
@@ -21,21 +21,55 @@
 
 @implementation FullBinaryTrees
 - (NSString *)stringForNodeCount:(NSInteger)count {
-    
-    NSArray *allNodes = [self nodeCreator:count];
-    NSMutableString *resultString = [[NSMutableString alloc] init];
+    NSMutableArray *allNodes = [[NSMutableArray alloc] initWithArray:[self nodeCreator:(count)]];
+    _counter = count-1;
+    NSMutableString *resultString = [[NSMutableString alloc] initWithString:@"[0,"];
+    _resultNodeValuesArray = [[NSMutableArray alloc] init];
     NSLog(@"%ld", allNodes.count);
     
     if (count == 0) {
-        [resultString appendString: @"[]"];
+        return @"[]";
     } else if (count == 1) {
-        [resultString appendString: @"[[0]]"];
+        return @"[[0]]";
     } else if (count % 2 == 0) {
-        [resultString appendString: @"[]"];
+        return  @"[]";
     }
     
+    for (NSInteger i = 0; i < allNodes.count; i++) {
+        Node *myNode = allNodes[i];
+        [self stringToResult:myNode];
+        [self stringToResult:myNode.left];
+        [self stringToResult:myNode.right];
+        [self stringToResult:myNode.left.left];
+        [self stringToResult:myNode.left.right];
+        [self stringToResult:myNode.right.left];
+        [self stringToResult:myNode.right.right];
+        [resultString appendString:[_resultNodeValuesArray componentsJoinedByString:@","]];
+        [resultString appendString:@"]"];
+        _counter = count-1;
+        [_resultNodeValuesArray removeAllObjects];
+        [resultString appendString:@"[0,"];
+    }
+    
+    
+    
+    resultString = [resultString substringToIndex:[resultString length] - 3];
     return resultString;
 }
+
+- (void)stringToResult: (Node*) node {
+    if (_counter != 0 && node != nil) {
+        if (node.left != nil && node.right != nil) {
+            [_resultNodeValuesArray addObject:@"0"];
+            [_resultNodeValuesArray addObject:@"0"];
+            _counter -= 2;
+        } else {
+            [_resultNodeValuesArray addObject:@"null"];
+            [_resultNodeValuesArray addObject:@"null"];
+        }
+    }
+}
+
     
 - (NSArray *)nodeCreator:(NSInteger)N {
     NSMutableArray *resultArray = [[NSMutableArray alloc]init];
