8b0d31ca498e24e32dfe6e535a1d4bb7b8a0f7ba
unTramp <57003672+unTramp@users.noreply.github.com>
2020-04-13 21:07:14 +0300

    feat: add task4.2 (WIP)
    Working on printing nodes function and having some issues. Wish to fix it asap.

--- RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
+++ RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
@@ -1,8 +1,63 @@
 #import "FullBinaryTrees.h"
 
+@interface Node : NSObject
+@property (nonatomic, assign) NSNumber *value;
+@property (nonatomic, strong) Node *left;
+@property (nonatomic, strong) Node *right;
+@end
+
+@implementation Node
+
+- (instancetype) initWithValue:(NSNumber *)value {
+    if (self = [super init]) {
+        self.value = value;
+        self.left = nil;
+        self.right = nil;
+    }
+    return self;
+}
+
+@end
+
 @implementation FullBinaryTrees
 - (NSString *)stringForNodeCount:(NSInteger)count {
-    // good luck
-    return 0;
+    
+    NSArray *allNodes = [self nodeCreator:count];
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    NSLog(@"%ld", allNodes.count);
+    
+    if (count == 0) {
+        [resultString appendString: @"[]"];
+    } else if (count == 1) {
+        [resultString appendString: @"[[0]]"];
+    } else if (count % 2 == 0) {
+        [resultString appendString: @"[]"];
+    }
+    
+    return resultString;
 }
+    
+- (NSArray *)nodeCreator:(NSInteger)N {
+    NSMutableArray *resultArray = [[NSMutableArray alloc]init];
+
+    if (N == 1) {
+        Node *myNode = [[Node alloc]init];
+        [resultArray addObject:myNode];
+    } else if (N % 2) {
+        for (NSInteger i = 0; i < N; ++i) {
+            NSArray *left = [self nodeCreator:i];
+            NSArray *right = [self nodeCreator:N - i - 1];
+            for (Node *leftChild in left) {
+                for (Node *rightChild in right) {
+                    Node *node = [[Node alloc]init];
+                    node.left = leftChild;
+                    node.right = rightChild;
+                    [resultArray addObject:node];
+                }
+            }
+        }
+    }
+    return [resultArray copy];
+}
+
 @end
